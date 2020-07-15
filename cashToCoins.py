126b4facca3dc5050a728b21919afb5382aed9e6
tannerb9 <tanner-brainard@hotmail.com>
2020-07-15 00:32:59 -0500

    Exercise completed.

--- cashToCoins.py
+++ cashToCoins.py
@@ -6,3 +6,13 @@ piggyBank = {
     "dimes": 0,
     "quarters": 0
 }
+
+piggyBank["quarters"] = int(dollarAmount / .25)
+remainder = round((dollarAmount - (piggyBank["quarters"] * .25)), 2)
+piggyBank["dimes"] = int(remainder / .10)
+remainder = remainder - (piggyBank["dimes"] * .1)
+piggyBank["nickels"] = int(remainder / .05)
+remainder = remainder - (piggyBank["nickels"] * .05)
+piggyBank["pennies"] = round(remainder / .01)
+
+print(piggyBank)
